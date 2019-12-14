7478fde21d7790d758540dd334e26697e92db208
Caijun Qin <47021574+Fennec2000GH@users.noreply.github.com>
2019-12-14 00:22:04 -0500

    current complete collection of utility functions for linked lists
    -includes equivalent "copy" functions from java.util.Arrays but applied to linked lists 
    -multiple methods of detecting loops 
    -basic functions to find size and get last Node

--- src/LinkedStructures/LinkedListUtils.java
+++ src/LinkedStructures/LinkedListUtils.java
@@ -1,81 +1,230 @@
 package LinkedStructures;
 import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public abstract class LinkedListUtils {
     //MEMBER FUNCTIONS
-    /** Sets <code>marker</code> to be <code>true</code> for all <code>Node</code> in linked list. Note that a linked
-     * list with no lopp is assumed.
-     * @param <T> </T>Data type of <code>Node</code>
+    /** Finds first occurrence of <code>Node</code> that contains given value for <code>data</code>
+     * @param <T> Data type of <code>Node</code>
      * @param head Head of given linked list
-     */
-    public static <T> void markAll(Node<T> head) {
+     * @param targetData Value for <code>data</code> to search for
+     * @return First <code>Node</code> to contain <code>targetData</code>, otherwise returns <code>null</code> if none exist
+     **/
+    public static <T> Node<T> get(Node<T> head, T targetData){
+        if(head == null)
+            return null;
+        Node<T> possibleLoop = LinkedListUtils.hashCycleDetection(head);
         Node<T> p = head;
-        while(p != null) {
-            p.setMarker(true);
+        int loopHeadCounter = 0;
+        while(p != null){
+            if(p.getData().equals(targetData))
+                return p;
+            if(p == possibleLoop)
+                ++loopHeadCounter;
+            if(loopHeadCounter >= 2)
+                return null;
             p = p.getNext();
         }
+        return null;
     }
 
-    /** Sets <code>marker</code> to be <code>false</code> for all <code>Node</code> in linked list. Note that a linked
-     * list with no lopp is assumed.
-     * @param <T> </T>Data type of <code>Node</code>
+    /** Finds first occurrence of <code>Node</code> that contains given value for <code>marker</code>
+     * @param <T> Data type of <code>Node</code>
      * @param head Head of given linked list
+     * @param targetMarker Value for <code>marker</code> to search for
+     * @return First <code>Node</code> to contain <code>targetMarker</code>, otherwise returns <code>null</code> if none exist
      */
-    public static <T> void unmarkAll(Node<T> head){
+    public static <T> Node<T> get(Node<T> head, boolean targetMarker){
+        if(head == null)
+            return null;
+        Node<T> possibleLoop = LinkedListUtils.hashCycleDetection(head);
         Node<T> p = head;
+        int loopHeadCounter = 0;
         while(p != null){
-            p.setMarker(false);
+            if(p.getMarker() == targetMarker)
+                return p;
+            if(p == possibleLoop)
+                ++loopHeadCounter;
+            if(loopHeadCounter >= 2)
+                return null;
             p = p.getNext();
         }
+        return null;
+    }
 
+    /** Finds first occurrence of <code>Node</code> that contains given value for both <code>data</code> and <code>marker</code>
+     * @param <T> Data type of <code>Node</code>
+     * @param head Head of given linked list
+     * @param targetData Value for <code>data</code> to search for
+     * @param targetMarker Value for <code>marker</code> to search for
+     * @return First <code>Node</code> to contain both <code>targetData</code> and <code>targetMarker</code>,
+     * otherwise returns <code>null</code> if none exist
+     */
+    public static <T> Node<T> get(Node<T> head, T targetData, boolean targetMarker){
+        if(head == null)
+            return null;
+        Node<T> possibleLoop = LinkedListUtils.hashCycleDetection(head);
+        Node<T> p = head;
+        int loopHeadCounter = 0;
+        while(p != null){
+            if(p.getData().equals(targetData) && p.getMarker() == targetMarker)
+                return p;
+            if(p == possibleLoop)
+                ++loopHeadCounter;
+            if(loopHeadCounter >= 2)
+                return null;
+            p = p.getNext();
+        }
+        return null;
+    }
 
+    /** Gets an unmodifiable list view of all <code>Node</code> with <code>marker</code> set to <code>true</code>,
+     * although sequential order of original linked list is not guaranteed.
+     * @param <T> Data type of <code>Node</code>
+     * @param head Head of given linked list
+     * @return Unmodifiable list view of <code>Node</code> marked <code>true</code>
+     */
+    public static <T> List<Node<T>> getMarked(Node<T> head){
+        HashSet<Node<T>> markedSet = new HashSet<>();
+        Node<T> p = head;
+        while(p != null && !markedSet.contains(p)){
+            if(p.getMarker())
+                markedSet.add(p);
+            p = p.getNext();
+        }
+        return markedSet.stream().parallel().collect(Collectors.toUnmodifiableList());
+    }
+
+    /** Gets an unmodifiable list view of all <code>Node</code> with <code>marker</code> set to <code>false</code>,
+     * although sequential order of original linked list is not guaranteed.
+     * @param <T> Data type of <code>Node</code>
+     * @param head Head of given linked list
+     * @return Unmodifiable list view of <code>Node</code> marked <code>false</code>
+     */
+    public static <T> List<Node<T>> getUnmarked(Node<T> head){
+        HashSet<Node<T>> unmarkedSet = new HashSet<>();
+        Node<T> p = head;
+        while(p != null && !unmarkedSet.contains(p)){
+            if(!p.getMarker())
+                unmarkedSet.add(p);
+            p = p.getNext();
+        }
+        return unmarkedSet.stream().parallel().collect(Collectors.toUnmodifiableList());
+    }
+
+    /** Sets <code>marker</code> to be <code>true</code> for all <code>Node</code> in linked list
+     * @param <T> </T>Data type of <code>Node</code>
+     * @param head Head of given linked list
+     * @param newMarker New <code>boolean</code> value to assign to <code>marker</code> for all <code>Node</code> in linked list
+     **/
+    public static <T> void markAll(Node<T> head, boolean newMarker) {
+        Node<T> possibleLoop = LinkedListUtils.hashCycleDetection(head);
+        Node<T> p = head;
+        int loopHeadCounter = 0;
+        while(p != null) {
+            if(p == possibleLoop)
+                ++loopHeadCounter;
+            if(loopHeadCounter >= 2)
+                break;
+            p.setMarker(newMarker);
+            p = p.getNext();
+        }
     }
 
     /** Copy of whole linked list
      * @param <T> </T>Data type of <code>Node</code>
      * @param head Head of given linked list
-     * @param deloop Whether to disconnect loop if encountered
-     * @return Deep copy of linked list led by <code>head</code>, with the exception of removing the loop if detected
-     * and <code>deloop</code> is <code>true</code>
+     * @return Deep copy of linked list led by <code>head</code>
      */
-    public static <T> Node<T> copy(Node<T> head, boolean deloop){
-        HashSet<Node<T>> catalog = new HashSet<>();
-        Node<T> prev = new Node<>(head), p = new Node<>(head.getNext());
-        Node<T> output = prev;
-        catalog.add(prev);
-        while(p != null && !catalog.contains(p)){
-            prev.setNext(p);
-            catalog.add(p);
-            p = p.getNext();
+    public static <T> Node<T> copy(Node<T> head){
+        if(head == null)
+            return null;
+        if(LinkedListUtils.isSingleton(head))
+            return new Node<>(head);
+        Node<T> prev = new Node<>(head), p = head.getNext(), output = prev, possibleLoop = LinkedListUtils.hashCycleDetection(head);
+        int loopHeadCounter = head == possibleLoop ? 1 : 0;
+        while(p != null){
+            prev.setNext(new Node<>(p));
             prev = prev.getNext();
+            if(loopHeadCounter >= 2)
+                break;
+            p = p.getNext();
+            if(p == possibleLoop)
+                ++loopHeadCounter;
+        }
+        return output;
+    }
+
+    /** Copy of linked list for a given <code>length</code>
+     * @param <T> Data type of <code>Node</code>
+     * @param head Head of given linked list
+     * @param length Number of <code>Node</code> in total to be in copy, starting from <code>head</code>
+     * @return Deep copy of linked list led by <code>head</code> for a given number of <code>Node</code> and will copy
+     * whole linked list if <code>length</code> is longer than actual length of linked list
+     */
+    public static <T> Node<T> copy(Node<T> head, int length){
+        try {
+            if (length <= -1)
+                throw new IllegalArgumentException("Length must be non-negative!");
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+        if(head == null || length == 0)
+            return null;
+        int counter = 1;
+        Node<T> prev = new Node<>(head), p = head.getNext(), output = prev, possibleLoop = LinkedListUtils.hashCycleDetection(head);
+        int loopHeadCounter = head == possibleLoop ? 1 : 0;
+        while(p != null && counter < length){
+            prev.setNext(new Node<>(p));
+            prev = prev.getNext();
+            if(loopHeadCounter >= 2)
+                break;
+            p = p.getNext();
+            if(p == possibleLoop)
+                ++loopHeadCounter;
+            ++counter;
         }
 
-        //if any loop detected must be kept
-        if(!deloop)
+        //in case length as parameter equals actual length of linked list and there is a loop present
+        if(p == possibleLoop)
             prev.setNext(p);
         return output;
     }
 
-//    /**
-//     * @param <T> Data type of <code>Node</code>
-//     * @param head Head of given linked list
-//     * @param length
-//     * @return
-//     */
-//    public static <T> Node<T> copy(Node<T> head, int length){
-//
-//    }
-//
-//    /**
-//     * @param <T> Data type of <code>Node</code>
-//     * @param head Head of given linked list
-//     * @param start
-//     * @param stop
-//     * @return
-//     */
-//    public static <T> Node<T> copyOfRange(Node<T> head, Node<T> start, Node<T> stop){
-//
-//    }
+    /** Copy of range of <code>Node</code> from a start <code>Node</code> and end <code>Node</code>. If tail of copy,
+     * denoted by <code>stop</code>, is not found, then copy goes all the way to the end of linked list.
+     * @param <T> Data type of <code>Node</code>
+     * @param head Head of given linked list
+     * @param start Head of copy (inclusive)
+     * @param stop Tail of copy (exclusive)
+     * @return Deep copy of linked list led by <code>head</code> within specified range
+     */
+    public static <T> Node<T> copyOfRange(Node<T> head, Node<T> start, Node<T> stop){
+        if(head == null || start == null || stop == null)
+            return null;
+        if(LinkedListUtils.isSingleton(head))
+            return new Node<>(head);
+        Node<T> prev = new Node<>(head), p = head.getNext(), output = prev, possibleLoop = LinkedListUtils.hashCycleDetection(head);
+        int loopHeadCounter = head == possibleLoop ? 1 : 0;
+        while (p != null && p != stop) {
+            prev.setNext(new Node<>(p));
+            prev = prev.getNext();
+            if(loopHeadCounter >= 2)
+                break;
+            p = p.getNext();
+            if(p == possibleLoop)
+                ++loopHeadCounter;
+        }
+        return output;
+    }
+
+    /** Finds out if linked list only contains one <code>Node</code>
+     * @param <T> Data type of <code>Node</code>
+     * @param head Head of given linked list
+     * @return Whether <code>head</code> is the only <code>Node</code> making up linked list
+     */
+    public static <T> boolean isSingleton(Node<T> head){ return head.getNext() == null; }
 
     /** Finds the size of linked list
      * @param <T> Data type of <code>Node</code>
@@ -232,11 +381,13 @@ public abstract class LinkedListUtils {
         return p;
     }
 
-    /** Detects cycle by
+    /** Detects cycle by storing and checking traversed <code>Node</code> in a <code>HashSet</code>
      * @param <T> Data type of <code>Node</code>
      * @param head Head of given linked list
      */
     public static <T> Node<T> hashCycleDetection(Node<T> head){
+        if(LinkedListUtils.isSingleton(head))
+            return null;
         HashSet<Node<T>> catalog = new HashSet<>();
         Node<T> p = head;
         while(p != null){
