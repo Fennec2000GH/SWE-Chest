fa14d4460941961743f2a8938d1cb78fa6e310ee
fennec2000 <qcaijun2013@gmail.com>
2020-02-25 15:12:15 -0500

    new file for practicing with std::includes from <algorithm>, and for initial commit connection test

---
+++ src/C++_STL_Practice/algorithms/includes_example.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+using namespace std;
+
+//std::includes
+/* (1) default
+ * template <class InputIterator1, class InputIterator2>
+ * bool includes ( InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2 );
+ */
+/* (2) pred
+ * template <class InputIterator1, class InputIterator2, class Compare>
+ * bool includes ( InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, Compare comp );
+ */
+
+int main(){
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
