f38c2af0a985bad3f3e6673bba04b233eb029f2e
fennec2000 <qcaijun2013@gmail.com>
2020-02-25 19:52:24 -0500

    completed examples for edge cases for std::rotate in <algorithm> library

--- src/C++_STL_Practice/algorithms/rotate_example.cpp
+++ src/C++_STL_Practice/algorithms/rotate_example.cpp
@@ -1,16 +1,49 @@
 #include <algorithm>
 #include <iostream>
 #include <iterator>
+#include <numeric>
 #include <vector>
 using namespace std;
 
 //std::rotate
-/*
- *
+/* template <class ForwardIterator>
+ * ForwardIterator rotate (ForwardIterator first, ForwardIterator middle, ForwardIterator last);
  */
 
 int main(){
+    //main vector
+    vector<char> v(26);
+    {
+        int intAlphabet[26]; // ASCII values of lowercase alphabet a-z
+        iota(intAlphabet, intAlphabet + 26, 97);
+        transform(intAlphabet, intAlphabet + 26, v.begin(), [](int a){ return (char)a; });
+    }
 
+    cout << "v: { ";
+    copy(v.cbegin(), v.cend(), ostream_iterator<char>(cout, ", "));
+    cout << "} " << endl;
+
+    //rotate such that 'q' is front
+    rotate(v.begin(), find(v.begin(), v.end(), 'q'), v.end());
+    cout << "v: { ";
+    copy(v.cbegin(), v.cend(), ostream_iterator<char>(cout, ", "));
+    cout << "} " << endl;
+    cout << boolalpha << "CHECK: 'q' is front ? " << (v.front() == 'q') << endl;
+
+    //rotate such that 'q' is last
+    rotate(v.begin(), v.begin() + 1, v.end());
+    cout << "v: { ";
+    copy(v.cbegin(), v.cend(), ostream_iterator<char>(cout, ", "));
+    cout << "} " << endl;
+    cout << boolalpha << "CHECK: 'q' is back ? " << (v.back() == 'q') << endl;
+
+    //rotate only sub-vector 'a, b, c' such that 'c' is front
+    rotate(v.begin(), find(v.begin(), v.end(), 'a'), v.end());
+    rotate(v.begin(), v.begin() + 2, v.begin() + 3);
+    cout << "v: { ";
+    copy(v.cbegin(), v.cend(), ostream_iterator<char>(cout, ", "));
+    cout << "} " << endl;
+    cout << boolalpha << "CHECK: 'c, b, a' is front ? " << equal(v.begin(), v.begin() + 3, new int[3]{'c', 'a', 'b'}) << endl;
 
     return 0;
 }
