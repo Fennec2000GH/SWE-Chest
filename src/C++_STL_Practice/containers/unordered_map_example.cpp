364e11d4ba22c00b7fb81410a331c6db9c2b74f6
fennec2000 <qcaijun2013@gmail.com>
2020-03-04 22:26:06 -0500

    completed custom tests for functions in std::unordered_map

--- src/C++_STL_Practice/containers/unordered_map_example.cpp
+++ src/C++_STL_Practice/containers/unordered_map_example.cpp
@@ -1,6 +1,7 @@
 
 #define  CATCH_CONFIG_MAIN
 #include "../../catch.hpp"
+#include <algorithm>
 #include <iostream>
 #include <iterator>
 #include <unordered_map>
@@ -181,8 +182,124 @@ void swap( unordered_map<Key,T,Hash,KeyEqual,Alloc>& lhs, unordered_map<Key,T,Ha
 void swap( unordered_map<Key,T,Hash,KeyEqual,Alloc>& lhs, unordered_map<Key,T,Hash,KeyEqual,Alloc>& rhs ) noexcept(//see below );
 */
 
+TEST_CASE("construction") {
+    unordered_map<int, string> m1;
+    cout << boolalpha << "m1.empty(): " << m1.empty() << endl;
+    CHECK(m1.empty());
+
+    unordered_map<char, long> m2({{'c', 42}, {'d', 12}, {'j', 32}, {'s', 88}});
+    cout << "m2: { ";
+    unordered_map<char, long>::const_iterator it2;
+    for(it2 = m2.cbegin() ; it2 != m2.cend(); it2++) { cout << "{" << it2 -> first << ", " << it2 -> second << "}, "; }
+    cout << "} " << endl;
+    CHECK_FALSE(m2.empty());
+    CHECK(m2.size() == 4);
+
+    unordered_map<char, long> m3(m2);
+    CHECK(m3.size() == m2.size());
+    cout << "m3: { ";
+    unordered_map<char, long>::const_iterator it3;
+    for(it3 = m3.cbegin(); it3 != m3.cend(); it3++) { cout << "{ " << it3 -> first << ", " << it3 -> second << "}, "; }
+    cout << "} " << endl;
 
 
+}
 
+TEST_CASE("basic member functions") {
+    unordered_map<unsigned, double> m({ {3, 4.123}, {6, 5.134}, {9, 3.444}, {4, 6.232} });
+    cout << "m: { ";
+    unordered_map<unsigned, double>::const_iterator it;
+    for(it = m.cbegin(); it != m.cend(); it++) { cout << "{ " << it -> first << ", " << it -> second << "}, "; }
+    cout << "} " << endl;
 
+    //testing insert
+    cout << "inserting new elements into m... " << endl;
+    m.insert({12, 4.112});
+    m.insert({16, 6.224});
+    cout << "values corresponding to keys 12 and 16 are " << m[12] << " and " << m[16] << ", respectively " << endl;
+    cout << "m: { ";
+    for(it = m.cbegin(); it != m.cend(); it++) { cout << "{ " << it -> first << ", " << it -> second << "}, "; }
+    cout << "} " << endl;
+    CHECK(m.size() == 6);
+    CHECK(m.at(12) == Approx(4.112).epsilon(0.01));
+    CHECK(m[16] == Approx(6.224).epsilon(0.01));
+
+    //testing insert_or_assign
+    cout << "possibly replacing value of key 16 with new value of 3.333 " << endl;
+    m.insert_or_assign(16, 3.333);
+    cout << "m: { ";
+    for(it = m.cbegin(); it != m.cend(); it++) { cout << "{ " << it -> first << ", " << it -> second << "}, "; }
+    cout << "} " << endl;
+    CHECK(m[16] == Approx(3.333).epsilon(0.01));
+
+    //testing erase
+    cout << "erasing elements from m... " << endl;
+    m.erase(12);
+    m.erase(m.find(16));
+    cout << "m: { ";
+    for(it = m.cbegin(); it != m.cend(); it++) { cout << "{ " << it -> first << ", " << it -> second << "}, "; }
+    cout << "} " << endl;
+    CHECK(m.count(12) == 0);
+    CHECK(m.count(16) == 0);
+    CHECK(m.size() == 4);
+
+    //tesing emplace
+    cout << "emplacing some new elements into m... " << endl;
+    m.emplace(make_pair<unsigned, double>(25, 2.567));
+    m.emplace_hint(m.find(16), make_pair<unsigned, double>(17, 1.222));
+    m.try_emplace(20, 4.234);
+    m.try_emplace(9, 4.466); //fails since 9 is already inserted
+    cout << "m: { ";
+    for(it = m.cbegin(); it != m.cend(); it++) { cout << "{ " << it -> first << ", " << it -> second << "}, "; }
+    cout << "} " << endl;
+    CHECK(m[25] == Approx(2.567).epsilon(0.01));
+    CHECK(m[20] == Approx(4.234).epsilon(0.01));
+    CHECK_FALSE(m[9] == Approx(4.466).epsilon(0.01));
+
+}
+
+TEST_CASE("non-member functions") {
+    unordered_map<long, char> m;
+    for(int k = 0; k < 10; k++) { m.emplace(k, char(97 + k)); }
+    cout << "m: { ";
+    unordered_map<long, char>::const_iterator it;
+    for(it = m.cbegin(); it != m.cend(); it++) { cout << "{ " << it -> first << ", " << it -> second << "}, "; }
+    cout << "} " << endl;
+    //C++20
+//    erase_if(m, [](long l, char c){ return l % 2 == 1; }); //erase all odd keys
+//    cout << "m: { ";
+//    for(it = m.cbegin(); it != m.cend(); it++) { cout << "{ " << it -> first << ", " << it -> second << "}, "; }
+//    cout << "} " << endl;
+//    CHECK(m.size() == 4);
+
+    unordered_map<long, char> n({ {25, 'd'} });
+    cout << "n: { ";
+    unordered_map<long, char>::const_iterator itn;
+    for(itn = n.cbegin(); itn != n.cend(); itn++) { cout << "{ " << itn -> first << ", " << itn -> second << "}, "; }
+    cout << "} " << endl;
+    cout << "swapping m and n... " << endl;
+    swap(m, n);
+    cout << "m: { ";
+    for(it = m.cbegin(); it != m.cend(); it++) { cout << "{ " << it -> first << ", " << it -> second << "}, "; }
+    cout << "} " << endl;
+    CHECK(m.size() == 1);
+    CHECK(n.size() == 10);
+
+}
+
+TEST_CASE("properties") {
+    unordered_map<char, char> m({ {'a', 'b'}, {'c', 'd'}, {'e', 'f'}, {'y', 'z'} });
+    cout << "m.bucket_count(): " << m.bucket_count() << endl;
+    cout << "m.max_bucket_count(): " << m.max_bucket_count() << endl;
+    cout << "m.bucket_size(0): " << m.bucket_size(0) << endl;
+    int index = m.bucket('y'); //index of bucket holding the key 'y'
+    cout << "m.bucket_size(index): " << m.bucket_size(index) << endl;
+    cout << "m.load_factor(): " << m.load_factor() << endl;
+    cout << "m.max_load_factor(): " << m.max_load_factor() << endl;
+    CHECK(m.bucket_count() > 0);
+    CHECK(m.max_bucket_count() > m.bucket_count());
+    CHECK(m.load_factor() > 0);
+    CHECK(m.max_load_factor() > m.load_factor());
+
+}
 
